02ac48fca951fca6b4c9f71cd28f916fb5e12ad0
Alexandre Malkov <sm1030@gmail.com>
2016-11-27 16:04:34 +0000

    Files rearranged

---
+++ Movies/Core/AppController.swift
@@ -0,0 +1,13 @@
+//
+//  AppController.swift
+//  Movies
+//
+//  Created by Alexandre Malkov on 27/11/2016.
+//  Copyright © 2016 Alexandre Malkov. All rights reserved.
+//
+
+import Foundation
+
+class AppController {
+    
+}
